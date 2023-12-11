6c00bddd96de3c9d48b4283b59e7a5a2a4f168ab
dinhquocthinh <139747275+dinhquocthinh@users.noreply.github.com>
2023-12-11 15:56:46 +0700

    web

---
+++ src/Entity/Category.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $CatName = null;
+
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class)]
+    private Collection $Products;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $CreateDate = null;
+
+    public function __construct()
+    {
+        $this->Products = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCatName(): ?string
+    {
+        return $this->CatName;
+    }
+
+    public function setCatName(string $CatName): static
+    {
+        $this->CatName = $CatName;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProducts(): Collection
+    {
+        return $this->Products;
+    }
+
+    public function addProduct(Product $product): static
+    {
+        if (!$this->Products->contains($product)) {
+            $this->Products->add($product);
+            $product->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduct(Product $product): static
+    {
+        if ($this->Products->removeElement($product)) {
+            // set the owning side to null (unless already changed)
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCreateDate(): ?\DateTimeInterface
+    {
+        return $this->CreateDate;
+    }
+
+    public function setCreateDate(\DateTimeInterface $CreateDate): static
+    {
+        $this->CreateDate = $CreateDate;
+
+        return $this;
+    }
+}
